a88c271dbbd073900e9057a2da5246ed25be9f1d
Yashesh Dasari <ydasari@uwaterloo.ca>
2026-02-28 04:42:20 -0500

    #7 Create user entity's module, controller, and service

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "./user.entity";
+import {Repository} from "typeorm";
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly usersRepository: Repository<User>,
+    ) {}
+
+    async create(email: string, password: string) {
+        const user = this.usersRepository.create({ email, password });
+        return this.usersRepository.save(user);
+    }
+}
