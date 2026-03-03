0543d3ce5ca6374ca52becee139a0141f3e2cf13
Yashesh Dasari <ydasari@uwaterloo.ca>
2026-03-03 10:22:55 -0500

    #7 Update the e2e test to verify creation of a user entity

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,10 +1,17 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { INestApplication } from "@nestjs/common";
+process.env.DB_HOST = 'localhost';
+process.env.DB_USER = 'postgres';
+process.env.DB_PASSWORD = 'Fus@2026';
+process.env.DB_NAME = 'clinical_test_db';
+
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { AppModule } from "../src/app.module";
+import { AppModule } from '../src/app.module';
+import { DataSource } from 'typeorm';
 
 describe('Users (e2e)', () => {
     let app: INestApplication;
+    let dataSource: DataSource;
 
     beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,15 +20,26 @@ describe('Users (e2e)', () => {
 
         app = moduleFixture.createNestApplication();
         await app.init();
+
+        dataSource = app.get(DataSource);
+        await dataSource.synchronize(true); // drops & recreates schema
+    });
+
+    afterAll(async () => {
+        await app.close();
     });
 
     it('POST /users should create a user', async () => {
         return request(app.getHttpServer())
             .post('/users')
             .send({
-                username: 'testuser',
-                password: 'testpass',
+                username: 'testUser',
+                password: 'testPassword',
+                email: 'user@test.com'
             })
-            .expect(201);
+            .expect(201)
+            .expect(res => {
+            expect(res.body).toHaveProperty('id')
+            });
     });
 });
